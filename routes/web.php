d27296bd2d8eda019b0b567bcea38345de7a3793
Eduardo Castellini <eduardo.castellini@hotmail.com>
2026-09-23 15:30:37 -0300

    adiciona rotas para gerenciamento de carteiras, depósitos, transferências e reversões de transações

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Http\Controllers\WalletController;
+use App\Http\Controllers\WalletDepositController;
+use App\Http\Controllers\WalletReversalController;
+use App\Http\Controllers\WalletTransferController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/wallet', [WalletController::class, 'show'])->name('wallet.show');
+    Route::post('/wallet/deposit', [WalletDepositController::class, 'store'])->name('wallet.deposit');
+    Route::post('/wallet/transfer', [WalletTransferController::class, 'store'])->name('wallet.transfer');
+    Route::post('/wallet/transactions/{transaction}/reverse', [WalletReversalController::class, 'store'])->name('wallet.transactions.reverse');
 });
 
 require __DIR__.'/settings.php';
